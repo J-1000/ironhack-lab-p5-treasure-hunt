59d1f2f110479f4a04cb28f3a6b198d4425535ab
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-02 22:37:16 +0200

    Done except Bonus, will work on it longer

--- src/game.js
+++ src/game.js
@@ -3,5 +3,63 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    for (let x = 0; x <= width; x += 100) {
+      for (let y = 0; y <= height; y += 100) {
+        line(x, 0, x, 1000);
+        line(0, y, 1000, y);
+      }
+    }
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = col;
+  }
+  draw() {
+    image(img, this.col, this.row, 100, 100);
+  }
+  moveUp() {
+    this.row -= 100
+  }
+  moveDown() {
+    this.row += 100
+  }
+  moveLeft() {
+    this.col -= 100
+  }
+  moveRight() {
+    this.col += 100
+  }
+}
+
+class Treasure {
+  constructor(col, row) {
+    this.col = Math.floor(Math.random() * 10) * 100
+    this.row = Math.floor(Math.random() * 10) * 100
+  }
+
+  // setRandomPosition(){
+  //   this.col = Math.floor(Math.random() * 10)*100;
+  //   this.row = Math.floor(Math.random() * 10)*100;
+  // }
+  draw() {
+    image(treasureImg, this.col, this.row, 100, 100);
+  }
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    player.moveUp();
+  }
+  if (keyCode === DOWN_ARROW) {
+    player.moveDown();
+  }
+  if (keyCode === LEFT_ARROW) {
+    player.moveLeft();
+  }
+  if (keyCode === RIGHT_ARROW) {
+    player.moveRight();
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 const game = new Game();
+const player = new Player(0, 0); // (0,0) = Initial position (col, row)
+const treasure = new Treasure(0, 0);
+let score = 0;
+
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  img = loadImage('assets/character-down.png', 0, 0);
+  treasureImg = loadImage('assets/treasure.png', 0, 0);
 }
 
 function draw() {
+  
+  clear()
+  treasure.draw();
   game.drawGrid();
+  player.draw();
+  if(player.row === treasure.row && player.col === treasure.col){
+    console.log(score += 1)
+  }
 }
