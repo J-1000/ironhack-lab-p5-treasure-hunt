d612023fe6dd571657d46c68464fa48bae7176d9
dionisio ugalde <dionisiougalde@iMac-de-dionisio-8.local>
2020-04-02 19:31:13 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,26 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    let canvas = createCanvas(WIDTH, HEIGHT);
+    canvas.parent("canvas");
+    for (let i = 0; i <= 10; i++) {
+      line(0, i * SQUARE_SIDE, WIDTH, i * SQUARE_SIDE);
+    }
+    for (let i = 0; i <= 10; i++) {
+      line(i * SQUARE_SIDE, 0, i * SQUARE_SIDE, HEIGHT);
+    }
+  }
+}
+class Player {}
+
+class Treasure {
+  constructor() {
+    this.xCord = Math.floor(Math.random() * 10) * 100;
+    this.yCord = Math.floor(Math.random() * 10) * 100;
+    this.img;
+    console.log(this.xCord, this.yCord);
+  }
+
+  drawTreasure() {
+    image(imgT, this.xCord, this.yCord, 100, 100);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,72 @@
 const game = new Game();
+let xCord = 0;
+let yCord = 0;
+let imgDown;
+let imgUp;
+let imgLeft;
+let imgRight;
+let img;
+let H = 100;
+let W = 100;
+let player;
+let treasure = new Treasure();
+let counter = 0;
 
-function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
-  canvas.parent("canvas");
+function preload() {
+  imgDown = loadImage("assets/character-down.png");
+  imgUp = loadImage("assets/character-up.png");
+  imgLeft = loadImage("assets/character-left.png");
+  imgRight = loadImage("assets/character-right.png");
+  img = imgDown;
+  imgT = loadImage("assets/treasure.png");
+
+  // treasure.preload();
 }
+function setup() {}
 
 function draw() {
+  clear();
+
   game.drawGrid();
+
+  image(img, xCord, yCord, W, H);
+
+  if (treasure.xCord == xCord && treasure.yCord == yCord) {
+    treasure = new Treasure();
+    counter += 100;
+  }
+  document.getElementById("sebasFanClub").innerHTML = counter;
+  treasure.drawTreasure();
 }
+
+function keyPressed() {
+  if (keyCode === 40) {
+    if (yCord !== HEIGHT * (9 / 10)) yCord += SQUARE_SIDE;
+    img = imgDown;
+  }
+
+  if (keyCode === 38) {
+    if (yCord !== 0) {
+      yCord -= SQUARE_SIDE;
+      img = imgUp;
+    }
+  }
+
+  if (keyCode === 37) {
+    if (xCord !== 0) {
+      xCord -= SQUARE_SIDE;
+      img = imgLeft;
+    }
+  }
+
+  if (keyCode === 39) {
+    if (xCord !== WIDTH * (9 / 10)) {
+      xCord += SQUARE_SIDE;
+      img = imgRight;
+    }
+  }
+}
+
+//create counter = 0
+// everytime you hit treasure, increase by 1
+// afterwards: set score = counter
