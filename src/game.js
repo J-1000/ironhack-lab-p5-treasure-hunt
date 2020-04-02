2623c6509171bd7d53bf9ec0e1ca2d83e4105bd8
psch0rr95 <sebastiannagl@icloud.com>
2020-04-02 17:01:05 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,51 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    for (let i = 0; i <= 1000; i += 100) {
+      line(i, 0, i, 1000);
+      line(0, i, 1000, i);
+    }
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+
+  moveUp() {
+    this.row -= 1;
+    lastKey = "up";
+  }
+
+  moveDown() {
+    this.row += 1;
+    lastKey = "down";
+  }
+
+  moveLeft() {
+    this.col -= 1;
+    lastKey = "left";
+  }
+
+  moveRight() {
+    this.col += 1;
+    lastKey = "right";
+  }
+
+  draw() {
+    image(img, this.col * 100, this.row * 100, 100, 100);
+  }
+}
+
+class Treasure {
+  constructor() {
+    this.col = Math.floor(Math.random() * 10);
+    this.row = Math.floor(Math.random() * 10);
+    console.log(this.row, this.col);
+  }
+
+  draw() {
+    image(imgT, this.col * 100, this.row * 100, 100, 100);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,64 @@
 const game = new Game();
+let lastKey = "down";
+let img = "";
+
+let counter = 0;
+
+let keyChecker = () => {
+  if (lastKey === "down") {
+    img = imgD;
+  } else if (lastKey === "up") {
+    img = imgU;
+  } else if (lastKey === "left") {
+    img = imgL;
+  } else {
+    img = imgR;
+  }
+  console.log(img);
+};
+function preload() {
+  imgD = loadImage("../assets/character-down.png");
+  imgL = loadImage("../assets/character-left.png");
+  imgR = loadImage("../assets/character-right.png");
+  imgU = loadImage("../assets/character-up.png");
+  imgT = loadImage("../assets/treasure.png");
+  console.log("preloaded");
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  player = new Player(0, 0);
+  treasure = new Treasure();
 }
 
 function draw() {
+  clear();
+  keyChecker();
   game.drawGrid();
+  player.draw();
+  treasure.draw();
+
+  document.getElementById("score").innerHTML = counter;
+
+  if (player.col === treasure.col && player.row === treasure.row) {
+    counter += 1;
+    treasure = new Treasure();
+  }
+}
+
+function keyPressed() {
+  console.log(keyCode);
+  if (keyCode === LEFT_ARROW) {
+    player.moveLeft();
+  }
+  if (keyCode === RIGHT_ARROW) {
+    player.moveRight();
+  }
+  if (keyCode === UP_ARROW) {
+    player.moveUp();
+  }
+  if (keyCode === DOWN_ARROW) {
+    player.moveDown();
+  }
 }
