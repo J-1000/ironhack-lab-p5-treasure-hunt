5d1cd747af1c92e10eb7b5c3c595f5175b6a1137
Jan Redmann <janredmann1@gmail.com>
2020-04-02 11:06:25 +0200

    initial commit

---
+++ src/game.js
@@ -0,0 +1,7 @@
+class Game {
+  drawGrid() {
+    // Iteration 1
+    // Draw the grid
+    // https://p5js.org/reference/#/p5/line
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,10 @@
+const game = new Game();
+
+function setup() {
+  let canvas = createCanvas(WIDTH, HEIGHT);
+  canvas.parent("canvas");
+}
+
+function draw() {
+  game.drawGrid();
+}
