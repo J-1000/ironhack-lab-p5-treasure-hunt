ed4035719826e05e5dad885cfca6ada4b59eaf59
Avtandil <a.shaburishvili@gmail.com>
2020-04-02 20:59:38 +0400

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,74 @@
 class Game {
   drawGrid() {
+    for (var x = 0; x <= width; x += width / 10) {
+      for (var y = 0; y <= height; y += height / 10) {
+        stroke(0);
+        strokeWeight(1);
+        line(x, 0, x, height);
+        line(0, y, width, y);
+      }
+    }
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
   }
 }
+//let avatar = loadImage('/assets/character-down.png');
+
+
+class Player {
+  constructor(col, row){
+    this.col = col;
+    this.row = row;
+  }
+
+  moveUp(){
+    if(this.row> 0){
+      this.row -= SQUARE_SIDE;
+    }
+    console.log(this.col, this.row);
+  }
+
+  moveDown(){
+    if(this.row <HEIGHT - SQUARE_SIDE ){
+      this.row += SQUARE_SIDE;
+    }
+    console.log(this.col, this.row);
+  }
+  moveLeft(){
+    if (this.col >0){
+      this.col -= SQUARE_SIDE;
+    }
+    console.log(this.col, this.row);
+  }
+moveRight(){
+  if(this.col<WIDTH - SQUARE_SIDE){
+    this.col += SQUARE_SIDE;
+  }
+  console.log(this.col, this.row);
+  console.log("moved right");"s"
+}
+drawPlayer(){
+  image(img,this.col, this.row, 100, 100);
+}
+}
+
+let newPlayer = new Player(0,0);
+console.log(newPlayer);
+
+
+class Treasure{
+  constructor() {
+    this.row ;
+    this.col;
+  }
+  randomPosition() {
+    this.row = Math.floor(Math.random() * 10)*100;
+    this.col = Math.floor(Math.random() *10)*100;
+  }
+  drawTreasure(){
+    image(treasureImage, this.row, this.col, 100,100)
+  }
+}
+let newTreasure = new Treasure();
+newTreasure.randomPosition();
--- src/main.js
+++ src/main.js
@@ -1,10 +1,50 @@
 const game = new Game();
 
+let treasureImage;
+let img;
+function preload(){
+  img = loadImage('../assets/character-down.png')
+  treasureImage = loadImage('../assets/treasure.png')
+}
+
+
+
+
+
 function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
+  let canvas = createCanvas(1000, 1000);
   canvas.parent("canvas");
 }
 
+
 function draw() {
+  clear();
   game.drawGrid();
+  newPlayer.drawPlayer();
+  newTreasure.drawTreasure()
+function keyPressed (){
+  if (keyIsDown(40)){
+    Player.moveDown();
+  }
+     console.log("arrow down");
+
+  if (keyIsDown(38)) {
+Player.moveUp();
+}
+    console.log("arrow up");
+   
+  if (keyIsDown(39)) {
+  
+    Player.moveRight();
+  }
+   console.log("arrow right");
+  
+  
+  if (keyIsDown(37)) {
+  
+    Player.moveLeft();
+  }
+    console.log("arrow left");
+
+}
 }
