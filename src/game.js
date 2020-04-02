1a70a0ab2e1c23ee577168e310855b89825246b4
yung-ting-chang <tina.yt.chang@protonmail.com>
2020-04-02 19:50:53 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,88 @@
+const size = 80;
+
+class Treasure {
+  constructor() {
+    this.col;
+    this.row;
+  }
+  setRandomPosition() {
+    let randomNumX = Math.floor(Math.random() * 10)
+    this.col = randomNumX;
+    let randomNumY = Math.floor(Math.random() * 10)
+    this.row = randomNumY;
+  }
+  drawTreasure(){
+    let xCord = 10 + this.col * size;
+    let yCord = 10 + this.row * size;
+    let h = 60;
+    let w = 60;
+    image(imgTreasure, xCord, yCord, h, w);
+    console.log('hey');
+  }
+}
+
+class Player {
+    constructor (col, row) {
+      this.col = col;
+      this.row = row;
+    }
+    moveUp() {
+      this.row -= 1;
+    }
+    moveDown() {
+      this.row += 1;
+    }
+    moveLeft(){
+      this.col -= 1;
+    }
+    moveRight(){
+      this.col += 1;
+    }
+    draw(){
+      let xCord = 10 + this.col * size;
+      let yCord = 10 + this.row * size;
+      let h = 60;
+      let w = 60;
+      image(img, xCord, yCord, h, w);
+    }
+  
+  }
+
+// const player = new Player(0,0) // (0,0) = Initial position (col, row)
+// player.moveDown() // Increase by 1 the value of player.row
+// player.moveDown() // Increase by 1 the value of player.row
+// player.moveRight() // Increase by 1 the value of player.col
+// console.log(player.col, player.row) // => 1,2
+
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    
+    const gridCounts = 10;
+    const max = size * gridCounts;
+    
+    for (let x = 0; x <= max; x += size) {
+      for (let y = 0; y <= max; y += size) {
+        rect(x, y, size, size);
+      }
+    }
+
+    // for (let i = 0; i <= max; i+= size) {
+    //   const x1 = i;
+    //   const y1 = 0;
+    //   const x2 = i;
+    //   const y2 = max;
+
+    //   line(x1, y1, x2, y2);
+    // }
+
+    // for (let i = 0; i <= max; i+= size) {
+    //   const x1 = 0;
+    //   const y1 = i;
+    //   const x2 = max;
+    //   const y2 = i;
+
+    //   line(x1, y1, x2, y2);
+    // }
+    
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,50 @@
+console.log(WIDTH);
+
 const game = new Game();
+const player = new Player(2, 0);
+const treasure = new Treasure();
+
+
+function preload() {
+  img = loadImage(
+          "assets/character-down.png"
+        );
+  imgTreasure = loadImage(
+          "assets/treasure.png"
+        );
+}
 
 function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
+  let canvas = createCanvas(800, 800); //==> (600,600) for example in px
   canvas.parent("canvas");
+  treasure.setRandomPosition()
 }
 
 function draw() {
   game.drawGrid();
+  player.draw();
+  treasure.drawTreasure();
+}
+
+
+//keyPressed is a p5 function: whenever you press a key this function is called
+function keyPressed() {
+  //keyCode is a P5 variable
+  clear();
+  if (keyCode === 40) {
+    player.moveDown();
+    console.log("arrow Down");
+  }
+  if (keyCode === 38) {
+    player.moveUp();
+    console.log("arrow up");
+  }
+  if (keyCode === 39) {
+    player.moveRight();
+    console.log("arrow right");
+  }
+  if (keyCode === 37) {
+    player.moveLeft();
+    console.log("arrow left");
+  }
 }
