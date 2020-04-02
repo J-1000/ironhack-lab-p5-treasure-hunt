6307f577f101edc03773d7053dfd870a6c5dfb2e
Victoria Kiseleva <111victoria.toria@gmail.com>
2020-04-02 18:09:42 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,58 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    for (var x = 0; x <= width; x += width / 10) {
+      for (var y = 0; y <= height; y += height / 10) {
+        stroke(0);
+        strokeWeight(1);
+        line(x, 0, x, height);
+        line(0, y, width, y);
+      }
+    }
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+
+  moveUp() {
+    this.row -= SQUARE_SIDE;
+    console.log("moveUp");
+  }
+  moveDown() {
+    this.row += SQUARE_SIDE;
+    console.log("moveDown");
+    console.log(this.col, this.row);
+  }
+  moveLeft() {
+    this.col -= SQUARE_SIDE;
+
+    console.log("moveLeft");
+  }
+  moveRight() {
+    this.col += SQUARE_SIDE;
+
+    console.log("moveRight");
+  }
+}
+
+function keyPressed() {
+  if (keyCode === 40) {
+    player.moveDown();
+    console.log("arrow Down");
+  }
+  if (keyCode === 38) {
+    player.moveUp();
+    console.log("arrow up");
+  }
+  if (keyCode === 37) {
+    player.moveLeft();
+    console.log("arrow left");
+  }
+  if (keyCode === 39) {
+    player.moveRight();
+    console.log("arrow right");
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 const game = new Game();
+let img;
+const player = new Player(0, 0);
+let DOWN_ARROW = 40;
+let UP_ARROW = 38;
+let RIGHT_ARROW = 39;
+let LEFT_ARROW = 37;
+
+function preload() {
+  player.img = loadImage("../assets/character-down.png");
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -7,4 +17,5 @@ function setup() {
 
 function draw() {
   game.drawGrid();
+  image(player.img, player.col, player.row, SQUARE_SIDE, SQUARE_SIDE);
 }
