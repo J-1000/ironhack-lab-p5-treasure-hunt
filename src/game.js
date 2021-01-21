026b57335091b86b0b18c0e10ab1909316a8eb53
Louisiane_Berlin <corentine.piette@gmail.com>
2021-01-21 20:42:53 +0100

    my commit

--- src/game.js
+++ src/game.js
@@ -1,7 +1,78 @@
 class Game {
-  drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+constructor() {
+  this.player = new Player();
+  this.treasure = new Treasure();
+}
+preload() {
+    this.player.image = loadImage('assets/Kenny.png');
+    this.treasure.image = loadImage('assets/bomb.png');
+}
+
+drawGrid() {
+  clear();
+  game.player.draw();
+  game.treasure.drawTreasure();
+  for (let i = 0;i<WIDTH;i++) {
+    line(0,i*SQUARE_SIDE,WIDTH,i*SQUARE_SIDE);
+    line(i*SQUARE_SIDE,0,i*SQUARE_SIDE,WIDTH);
+    }
   }
 }
+
+class Player {
+  constructor() {
+    this.col = 1 ;
+    this.row = 1 ;
+    this.image;
+}
+moveUp() {
+    this.row -= 1;
+}
+moveDown() {
+    this.row += 1;
+}
+moveLeft() {
+    this.col -= 1;
+}
+moveRight() {
+    this.col += 1;
+}
+
+ draw() {
+        clear();
+        this.col = constrain(this.col, 1, 10)
+        this.row = constrain(this.row, 1, 10)
+        image(this.image, (this.col-1)*SQUARE_SIDE, (this.row-1)*SQUARE_SIDE, SQUARE_SIDE, SQUARE_SIDE)
+
+  // if (keyIsDown(38)) {
+  //     this.moveUp()
+  // }
+  // if (keyIsDown(40)) {
+  //     this.moveDown()
+  // }
+  // if (keyIsDown(38)) {
+  //     this.moveLeft()
+  // }
+  // if (keyIsDown(39)) {
+  //     this.moveRight()
+  // }
+  console.log(`${this.row} and ${this.col}`)
+}
+}
+
+class Treasure {
+    constructor()  {
+    this.col = 1;
+    this.row = 1;
+    this.image;
+    }
+
+  setRandomPosition() {
+    this.col = 1 + Math.floor(Math.random()*9);
+    this.row = 1 + Math.floor(Math.random()*9);
+  }
+
+  drawTreasure() {
+    image(this.image,(this.col-1)*SQUARE_SIDE,(this.row-1)*SQUARE_SIDE, SQUARE_SIDE, SQUARE_SIDE);
+  }
+  }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,31 @@ const game = new Game();
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  game.treasure.setRandomPosition();
 }
 
 function draw() {
   game.drawGrid();
 }
+
+function preload() {
+  game.preload();
+}
+
+
+function keyPressed() {
+  console.log(keyCode);
+  if (keyCode === 38) {
+      game.player.moveUp();
+  }
+  if (keyCode === 40) {
+      game.player.moveDown();
+  }
+  if (keyCode === 37) {
+      game.player.moveLeft();
+  }
+  if (keyCode === 39) {
+      game.player.moveRight();
+  }
+}
+
