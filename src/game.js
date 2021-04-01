90b97280af7b25b337e070dadb44dd0f6223c452
mhsngr <markus.heisinger@gmail.com>
2021-04-01 20:11:07 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,61 @@
 class Game {
+  constructor() {
+    this.player = new Player(4, 4);
+    this.treasure = new Treasure();
+  }
+  preload () {
+    this.player.image = loadImage('../assets/character-down.png');
+    this.treasure.image = loadImage('../assets/treasure.png');
+    this.treasure.setRandomPosition();
+  }
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    for (let i = 100; i < 1000; i += 100) {
+      line(i, 0, i, 1000)
+      line(0, i, 1000, i)
+    }
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.image;
+  }
+  moveUp() {
+    this.image = loadImage('../assets/character-up.png');
+    this.row--;
+  }
+  moveDown() {
+    this.image = loadImage('../assets/character-down.png');
+    this.row++;
+  }
+  moveLeft() {
+    this.image = loadImage('../assets/character-left.png');
+    this.col--;
+  }
+  moveRight() {
+    this.image = loadImage('../assets/character-right.png');
+    this.col++;
+  }
+  draw() {
+    this.col = constrain(this.col, 0, 9);
+    this.row = constrain(this.row, 0, 9);
+    image(this.image, this.col * 100, this.row * 100, 100, 100)
+  }
+}
+
+class Treasure {
+  constructor() {
+    this.col;
+    this.row;
+    this.image;
+  }
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * 10);
+    this.row = Math.floor(Math.random() * 10);
+  }
+  draw() {
+    image(this.image, this.col * 100, this.row * 100, 100, 100)
   }
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,31 @@ function setup() {
   canvas.parent("canvas");
 }
 
+function preload() {
+  game.preload();
+}
+
 function draw() {
+  clear();
   game.drawGrid();
+  game.treasure.draw();
+  game.player.draw();
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    game.player.moveUp()
+  }
+  if (keyCode === DOWN_ARROW) {
+    game.player.moveDown()
+  }
+  if (keyCode === LEFT_ARROW) {
+    game.player.moveLeft()
+  }
+  if (keyCode === RIGHT_ARROW) {
+    game.player.moveRight()
+  }
+  if (game.player.col === game.treasure.col && game.player.row === game.treasure.row) {
+    game.treasure.setRandomPosition();
+  }
 }
