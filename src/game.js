01e3a610ea258a7f7b1b207a8764ccf5194706c4
helenetrommelen <helenetrommelen@gmail.com>
2021-04-01 16:28:39 +0200

    finished iteration 1

--- src/game.js
+++ src/game.js
@@ -3,5 +3,15 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
-  }
+    push()
+    for (let i=0; i<11; i++){
+      let rect=0;
+      const xstart= width/2-(rect*5)
+      const xend = width/2+(rect*5)
+      line(xstart, i*rect, xend, i*rect)
+      line(xstart+i*rect, 0, xstart+i*rect, rect*10)
+    }
+    pop()
+
+}
 }
