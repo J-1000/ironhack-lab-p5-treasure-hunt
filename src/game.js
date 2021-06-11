6a354b8fbe3cd72c981b2fdda81e0ea9dc93e471
Mangonando <juan.f.ramirezv@gmail.com>
2021-06-11 18:09:55 +0200

    unfinished. grid done. viking and treasure shows. not moving. lots of changes from Ahmed code

--- src/game.js
+++ src/game.js
@@ -1,7 +1,108 @@
 class Game {
+  constructor() {
+    this.player = new Player(0, 0);
+    this.treasure = new Treasure(500, 500);
+    this.score = 0;
+  }
+
+  preload() {
+    this.player.playerPreload('D');
+    this.treasure.treasurePreload();
+  }
+
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    clear();
+    stroke(0);
+    for(let i= 0; i<=1000; i+=100)
+    {
+      line(0, i, WIDTH, i); //horizontal lines - y changes
+      line(i, 0, i, HEIGHT); //vertical lines - x changes    
+    }
+    this.player.draw();
+  }
+  drawGame() {
+    game.player.draw();
+    game.treasure.draw();
+
+    if(game.player.col === game.treasure.col && game.player.row === game.treasure.row)
+    {
+      game.treasure.setRandomPosition();
+      this.score += 1;
+    }
+    document.querySelector('h2').querySelector('span').innerText = this.score;  
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    // this.width = 100;
+    // this.height = 100;
+    this.image;
+  }
+  moveRight () {
+    this.col += 100;
+    this.playerPreload('R')
+  }
+  moveDown () {
+    this.row += 100;
+    this.playerPreload('D')
+  }
+  moveLeft () {
+    this.col -= 100;
+    this.playerPreload('L')
+  }
+  moveUp () {
+    this.row -= 100;
+    this.playerPreload('U')
+  }
+
+  playerPreload(direction) {
+    switch (direction) {
+      case 'U':
+        this.image = loadImage('assets/character-up.png');
+        break;
+      case 'D':
+        this.image = loadImage('assets/character-down.png');
+        break;
+      case 'L':
+        this.image = loadImage('assets/character-left.png');
+        break;
+      case 'R':
+        this.image = loadImage('assets/character-right.png');
+        break;
+    }
+  }
+  
+  draw() {
+    this.row = constrain(this.row, 0, (HEIGHT - 100))
+    this.col = constrain(this.col, 0, (WIDTH - 100))
+    image(this.image, this.col, this.row, 100, 100)
+  }
+}
+
+class Treasure {
+  constructor(col, row) {
+    this.row = row;
+    this.col = col;
+    this.image;
+  }
+
+  treasurePreload() {
+    console.log('treasuePreload called');
+    this.image = loadImage('assets/treasure.png');
+  }
+
+  setRandomPosition() {
+    console.log("setRandomPosition is called");
+    this.row = (Math.floor(Math.random() * 10)) * 100;
+    this.col = (Math.floor(Math.random() * 10)) * 100;
+    console.log(this.row);
+    console.log(this.col);
+  }
+
+  draw() {
+    image(this.image, this.col, this.row, 100, 100)
   }
 }
--- src/main.js
+++ src/main.js
@@ -7,4 +7,43 @@ function setup() {
 
 function draw() {
   game.drawGrid();
+  game.drawGame();
 }
+// 	background(255);
+// 	for (let x = 0; x <= width; x += width / 10) {
+// 		for (let y = 0; y <= height; y += height / 10) {
+// 			stroke(0);
+// 			strokeWeight(1);
+// 			line(x, 0, x, height);
+// 			line(0, y, width, y);
+// 		}
+
+// 	}
+// }
+
+function preload() {
+  game.preload();
+}
+
+function keyPressed() {
+  //console.log(keyCode);
+  if (keyCode === 38) {
+      game.player.moveUp();
+  }
+  if (keyCode === 40) {
+      game.player.moveDown();
+  }
+  if (keyCode === 37) {
+      game.player.moveLeft();
+  }
+  if (keyCode === 39) {
+      game.player.moveRight();
+  }
+}
+// function preload() {
+//   img = loadImage('assets/character-down.png')
+//   tr_img = loadImage('assets/treasure.png')
+// }
+
+
+// }
