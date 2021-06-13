ba1917e2cef7c1e20ce508e6dd23fe8db8425806
Alan Sanchez <alnwolff@gmail.com>
2021-06-13 14:18:04 +0200

    correct functionality, add player 2 and show the score separately

--- src/game.js
+++ src/game.js
@@ -1,7 +1,186 @@
 class Game {
+
+  constructor () {
+    //create treasure and score
+    this.score = 0;
+    this.score2 = 0;
+  }
+
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    clear();
+    stroke(0);
+
+    for(let i= 0; i<=1000; i+=100)
+  {
+    line(0, i, WIDTH, i);   //horizontal lines - y changes
+    line(i, 0, i, HEIGHT);  //vertical lines - x changes   
+  }
+  }
+
+  setup() {
+    this.player = new Player(0,0);
+    this.treasure = new Treasure();
+    this.player2 = new Player2(900,900);
+    this.player.image = loadImage('../assets/character-down.png');
+    this.treasure.image = loadImage('../assets/treasure.png');
+    this.player2.image = loadImage('../assets/character-up.png');
+  }
+
+  preload(){
+    
+  }
+
+  drawGame() {
+    this.player.draw()
+    this.player2.draw()
+    this.treasure.drawTreasure()
+
+    if ( this.player.col === this.treasure.col && this.player.row === this.treasure.row ) {
+
+      this.treasure.setRandomPosition();
+
+      this.score += 1;
+    }
+    if ( this.player2.col === this.treasure.col && this.player2.row === this.treasure.row ) {
+
+      this.treasure.setRandomPosition();
+
+      this.score2 += 1;
+    }
+    document.querySelector('#player1score').querySelector('span').innerText = this.score; 
+    document.querySelector('#player2score').querySelector('span').innerText = this.score2; 
+
+  }
+}
+
+class Player {
+
+  constructor(col,row) {
+
+    this.col = col;
+    this.row = row;
+    this.image;
+
+  }
+
+  moveUp() {
+    this.row -= 100;
+    this.playerPreload('U')
+  }
+
+  moveDown() {
+    this.row += 100;
+    this.playerPreload('D')
+  }
+
+  moveLeft() {
+    this.col -= 100;
+    this.playerPreload('L')
+  }
+
+  moveRight() {
+    this.col += 100;
+    this.playerPreload('R')
+  }
+
+  playerPreload (direction) {
+
+    switch(direction) {
+      case 'U':
+        this.image = loadImage('assets/character-up.png');
+        break;
+      case 'D':
+        this.image = loadImage('assets/character-down.png');
+        break;
+      case 'L':
+        this.image = loadImage('assets/character-left.png');
+        break;
+      case 'R':
+        this.image = loadImage('assets/character-right.png')
+    }
+
+  }
+
+  draw() {
+    this.row = constrain(this.row, 0, (HEIGHT - 100))
+    this.col = constrain(this.col, 0, (WIDTH - 100))
+    image(this.image, this.col, this.row, 100, 100);
+  }
+}
+
+
+class Treasure {
+
+  constructor () {
+    this.col = (Math.floor(Math.random() * 9) * 100);
+    this.row = (Math.floor(Math.random() * 9) * 100);
+    this.image;
+  }
+
+  setRandomPosition() {
+    this.col = (Math.floor(Math.random() * 9) * 100);
+    this.row = (Math.floor(Math.random() * 9) * 100);
+  }
+
+  drawTreasure() {
+    image(this.image, this.col, this.row, 100, 100);
+  }
+}
+
+
+// add second player!!
+
+class Player2 {
+
+  constructor(col,row) {
+
+    this.col = col;
+    this.row = row;
+    this.image;
+
+  }
+
+  moveUp() {
+    this.row -= 100;
+    this.player2Preload('U')
+  }
+
+  moveDown() {
+    this.row += 100;
+    this.player2Preload('D')
+  }
+
+  moveLeft() {
+    this.col -= 100;
+    this.player2Preload('L')
+  }
+
+  moveRight() {
+    this.col += 100;
+    this.player2Preload('R')
+  }
+
+  player2Preload (direction) {
+
+    switch(direction) {
+      case 'U':
+        this.image = loadImage('assets/character-up.png');
+        break;
+      case 'D':
+        this.image = loadImage('assets/character-down.png');
+        break;
+      case 'L':
+        this.image = loadImage('assets/character-left.png');
+        break;
+      case 'R':
+        this.image = loadImage('assets/character-right.png')
+    }
+
+  }
+
+  draw() {
+    this.row = constrain(this.row, 0, (HEIGHT - 100))
+    this.col = constrain(this.col, 0, (WIDTH - 100))
+    image(this.image, this.col, this.row, 100, 100);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,116 @@
 const game = new Game();
 
+function preload() {
+  game.preload();
+}
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  game.setup()
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  game.drawGame();
 }
+
+function keyPressed() {
+  if (keyCode === 38) {
+    game.player.moveUp();
+  }
+  if (keyCode === 40) {
+    game.player.moveDown();
+
+  }
+  if (keyCode === 37) {
+    game.player.moveLeft();
+  }
+  if (keyCode === 39) {
+    game.player.moveRight();
+  }
+  if (keyCode === 87) {
+    game.player2.moveUp();
+  }
+  if (keyCode === 83) {
+    game.player2.moveDown();
+
+  }
+  if (keyCode === 65) {
+    game.player2.moveLeft();
+  }
+  if (keyCode === 68) {
+    game.player2.moveRight();
+  }
+}
+
+
+
+
+
+// OOP
+
+// let img;
+// let tr_img;
+// let player_x = 0;
+// let player_y = 0;
+// let treasure_x = 500;
+// let treasure_y = 500;
+// let score = 0;
+
+// function setup() {
+//   console.log("setup is called");
+//   let canvas = createCanvas(WIDTH, HEIGHT);
+//   canvas.parent("canvas");
+// }
+
+// function draw() {
+//   console.log("draw is called");
+//   clear();
+//   stroke(0);
+
+//   for(let i= 0; i<=1000; i+=100)
+//   {
+//     line(0, i, WIDTH, i);   //horizontal lines - y changes
+//     line(i, 0, i, HEIGHT);  //vertical lines - x changes   
+//   }
+
+//   image(img, player_x, player_y, 100, 100);
+//   image(tr_img, treasure_x, treasure_y, 100, 100);
+
+//   if(player_x === treasure_x && player_y === treasure_y)
+//   {
+//     treasure_x = (Math.floor(Math.random() * 10)) * 100;
+//     treasure_y = (Math.floor(Math.random() * 10)) * 100;
+//     score += 1;
+//   }
+//   document.querySelector('h2').querySelector('span').innerText = score; 
+
+// }
+
+// function preload() {
+//   console.log("preload is called");
+//   img = loadImage('assets/character-down.png');
+//   tr_img = loadImage('assets/treasure.png');
+// }
+
+// function keyPressed() {
+//   if (keyCode === 38) {
+//     player_y -= 100;
+//     img = loadImage('assets/character-up.png');
+//   }
+//   if (keyCode === 40) {
+//     player_y += 100;
+//     img = loadImage('assets/character-down.png');
+//   }
+//   if (keyCode === 37) {
+//     player_x -= 100;
+//     img = loadImage('assets/character-left.png');
+//   }
+//   if (keyCode === 39) {
+//     player_x += 100;
+//     img = loadImage('assets/character-right.png');
+//   }
+
+// }
