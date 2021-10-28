77481b3dc5fcf63ca6cdc585d3202620cafcab50
John Doe <adamalfraih@gmail.com>
2021-10-28 20:01:15 +0200

    complete up to iteration 4

--- src/game.js
+++ src/game.js
@@ -1,7 +1,58 @@
 class Game {
+  constructor() {
+		this.player = new Player(0,0)
+	}
+
   drawGrid() {
     // Iteration 1
-    // Draw the grid
+    // horizontal line
+    for (let i = 0; i < 1100; i += 100) {
+      line(0, i, 1000, i);
+      line(i, 0, i, 1000);
+    } 
     // https://p5js.org/reference/#/p5/line
   }
+  preload() {
+		this.player.image = loadImage('character-up.png')
+	}
+	draw() {
+		clear()
+		this.player.draw()
+	}
 }
+
+class Player {
+  constructor(col, row){
+  this.col = col
+  this.row = row
+  this.width = 100;
+  this.height = 100;
+  this.image = loadImage ('../assets/character-down.png');
+  }
+  moveRight() {
+		this.row += 1
+	}
+	moveLeft() {
+		this.row -= 1
+	}
+	moveUp() {
+		this.col -= 1
+	}
+	moveDown() {
+		this.col += 1
+	}
+
+
+  draw() {
+		image(this.image, 100*this.row, 100*this.col, 100, 100)
+		// if you keep 'd' (keyCode 68)  pressed the player keeps moving to the right
+
+		if (keyIsDown(68)) {
+			this.moveRight()
+		}
+	}
+}
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-const game = new Game();
-
+let game;
+//const player = new Player(0,0);
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
+  game = new Game();
   canvas.parent("canvas");
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  game.player.draw();
+  game.player.moveDown();
+  
+  
+
 }
+
