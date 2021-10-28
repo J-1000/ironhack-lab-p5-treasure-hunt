c2f5afaa990c20723546939b95246cafc05cdfcd
Henrik Thiess <34238591+HenrikThi@users.noreply.github.com>
2021-10-28 17:17:48 +0200

    Adds game with basic scoring

--- src/game.js
+++ src/game.js
@@ -1,7 +1,92 @@
 class Game {
+  constructor(player, treasure) {
+    this.player = player;
+    this.treasure = treasure;
+    this.score = 0;
+  }
   drawGrid() {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+
+    for (let i = 0; i < 11; i++) {
+      line(0, i * 100, 1000, i * 100);
+      line(i * 100, 0, i * 100, 1000);
+    }
+  }
+
+  drawAssets(){
+    this.treasure.draw();
+    this.player.draw();
+  }
+
+  checkCollisions() {
+    if(this.treasure.col === this.player.col && this.treasure.row === this.player.row){
+      this.treasure.setRandomPosition();
+      document.querySelector('h1').innerText = `Score: ${++this.score * 100}`;
+    }
+    
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.image = loadImage("../assets/character-down.png");
+  }
+
+  moveUp() {
+    if (this.outOfGridBoundary(this.col, this.row - 1)) return;
+
+    this.image = loadImage("../assets/character-up.png");
+    this.row--;
+  }
+
+  moveDown() {
+    if (this.outOfGridBoundary(this.col, this.row + 1)) return;
+    this.image = loadImage("../assets/character-down.png");
+    this.row++;
+  }
+
+  moveLeft() {
+    if (this.outOfGridBoundary(this.col - 1, this.row)) return;
+
+    this.image = loadImage("../assets/character-left.png");
+    this.col--;
+  }
+
+  moveRight() {
+    if (this.outOfGridBoundary(this.col + 1, this.row)) return;
+
+    this.image = loadImage("../assets/character-right.png");
+    this.col++;
+  }
+
+  draw() {
+    console.log(this.col, this.row);
+    image(this.image, this.col * 100, this.row * 100, 100, 100);
+  }
+
+  outOfGridBoundary(col, row) {
+    return col >= 10 || row >= 10 || col < 0 || row < 0;
+  }
+}
+
+class Treasure {
+  constructor() {
+    this.image = loadImage("../assets/treasure.png");
+    this.col;
+    this.row;
+    this.setRandomPosition();
+  }
+
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * 10);
+    this.row = Math.floor(Math.random() * 10);
+  }
+
+  draw() {
+    image(this.image, this.col * 100, this.row * 100, 100, 100);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
-const game = new Game();
+let game;
+let player;
+let treasure;
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  game = new Game(new Player(0, 0), new Treasure());
+  // player = new Player(0, 0);
+  // treasure = new Treasure();
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  game.drawAssets();
+  game.checkCollisions();
+}
+
+function keyPressed() {
+  if (keyCode === 39) {
+    game.player.moveRight();
+  }
+  if (keyCode === 37) {
+    game.player.moveLeft();
+  }
+  if (keyCode === 38) {
+    game.player.moveUp();
+  }
+  if (keyCode === 40) {
+    game.player.moveDown();
+  }
 }
