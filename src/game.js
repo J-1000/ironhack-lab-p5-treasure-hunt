74e09b125981f943cb1114b938f281871f4a99b8
Johannes von Kessel <johannes.v.kessel@web.de>
2021-10-28 19:04:55 +0200

    update

--- src/game.js
+++ src/game.js
@@ -1,7 +1,54 @@
 class Game {
   drawGrid() {
+    
+    for (let i=0; i<= 1000; i = i +100){
+      line(0, i, 1000, i); 
+    }
+    for (let i=0; i<= 1000; i = i +100){
+      line(i, 0, i, 1000); 
+    }
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
   }
 }
+
+class Player {
+
+  preload() {
+
+    this.image = loadImage('../assets/character-down.png')
+    }
+
+  constructor(){
+    this.width = 100;
+		this.height = 100;
+		this.image;
+    this.x = 0;
+	  this.y = 0;
+  }
+  draw (){
+
+    clear()
+
+    image(this.image, this.x, this.y, this.width, this.height)
+
+    // image(this.image, 500, 500, 100, 100)
+  
+
+  }
+  moveRight() {
+		this.x += 100
+	}
+	moveLeft() {
+		this.x -= 100
+	}
+	moveUp() {
+		this.y -= 100
+	}
+	moveDown() {
+		this.y += 100
+	}
+  
+}
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 const game = new Game();
+const player = new Player ();
+
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -7,4 +9,28 @@ function setup() {
 
 function draw() {
   game.drawGrid();
+  player.draw();
+}
+
+function preload(){
+  player.preload()
+}
+
+function keyPressed() {
+	if (keyCode === 39) {
+		// move the player to the right
+		player.moveRight();
+	}
+	if (keyCode === 37) {
+		// move the player to the left
+		player.moveLeft();
+	}
+	if (keyCode === 38) {
+		// move the player up
+		player.moveUp();
+	}
+	if (keyCode === 40) {
+		// move the player down
+		player.moveDown();
+	}
 }
