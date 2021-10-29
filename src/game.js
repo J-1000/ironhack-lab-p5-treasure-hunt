16e262895fdf5b4fb49de7ef6308eb2bb970d668
Anna Morozova <a.morozova.nrw@gmail.com>
2021-10-29 17:29:49 +0200

    grid + player

--- src/game.js
+++ src/game.js
@@ -1,7 +1,40 @@
+// create class(template/new block of code) Player - encapsulation (key principle of OOP)
+// data and functionality of object go into class - this is object template
+// function definition inside the class is constructor(){} - it is an object set up
+
 class Game {
-  drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+  constructor(player) {
+    this.player = new Player(0, 0); 
   }
+
+  preload() {
+  this.playerImage = loadImage('../assets/character-down.png')
+  }
+  
+  drawGrid() {
+    for (let i = 0; i <= 10; i++) {
+      let x = i * SQUARE_SIDE;
+      stroke(130, 100, 0)
+      line(0, x, 1000, x);
+      line (x, 0 , x ,1000)
+    }
+  }
+
+  drawPlayer() {
+  this.player.drawPlayer();
+  }
+
+}
+
+class Player {
+constructor(col, row) {
+this.col = col;
+this.row = row;
+}
+
+drawPlayer() {
+  image(game.playerImage, this.row*50, this.col*100, 100, 100);
+  console.log('hello')
+}
+
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
+// declare variables(objects) for the game
 const game = new Game();
 
-function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
-  canvas.parent("canvas");
+function preload(){
+  game.preload();
 }
 
+// this is object instance
+function setup() {
+  let canvas = createCanvas(WIDTH, HEIGHT);
+  canvas.parent('canvas');
+}
+
+// commands
 function draw() {
   game.drawGrid();
+  game.drawPlayer();
 }
