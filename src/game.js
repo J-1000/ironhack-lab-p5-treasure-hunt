613e10ba845a8223eb31fe37363380b3d01b91ba
John Doe <adamalfraih@gmail.com>
2021-10-29 18:05:47 +0200

    complete bonus

--- src/game.js
+++ src/game.js
@@ -1,23 +1,33 @@
+const scoreCount = document.querySelector('h2')
+scoreCount.innerText = 'Score: ';
+
+
 class Game {
   constructor() {
 		this.player = new Player(0,0)
+		this.treasure = new Treasure()
+		
 	}
 
   drawGrid() {
     // Iteration 1
     // horizontal line
-    for (let i = 0; i < 1100; i += 100) {
-      line(0, i, 1000, i);
-      line(i, 0, i, 1000);
+		clear()
+    for (let i = 0; i <= WIDTH; i += 100) {
+      line(0, i, WIDTH, i)
+      line(i, 0, i, HEIGHT)
     } 
     // https://p5js.org/reference/#/p5/line
   }
   preload() {
-		this.player.image = loadImage('character-up.png')
+		this.player.image = loadImage('../assets/character-down.png')
+		this.treasure.image = loadImage('../assets/treasure.png')
+		this.treasure.treasureReset()
+
 	}
 	draw() {
-		clear()
 		this.player.draw()
+		this.treasure.draw()
 	}
 }
 
@@ -27,21 +37,39 @@ class Player {
   this.row = row
   this.width = 100;
   this.height = 100;
-  this.image = loadImage ('../assets/character-down.png');
+  this.image;
+  this.score = 0;
   }
+
   moveRight() {
-		this.row += 1
+		if (this.row < 9){
+		this.row += 1	
+		} 
+		
+		this.image = loadImage('../assets/character-right.png')
+		this.check()
 	}
 	moveLeft() {
-		this.row -= 1
+		if (this.row > 0) {
+			this.row -= 1
+		}
+		this.image = loadImage('../assets/character-left.png')
+		this.check()
 	}
 	moveUp() {
-		this.col -= 1
+		if (this.col > 0 ) {
+			this.col -= 1
+		}
+		this.image = loadImage('../assets/character-up.png')
+		this.check()
 	}
 	moveDown() {
-		this.col += 1
+		if (this.col <9 ) {
+			this.col += 1
+		}
+		this.image = loadImage('../assets/character-down.png')
+		this.check()
 	}
-
 
   draw() {
 		image(this.image, 100*this.row, 100*this.col, 100, 100)
@@ -51,7 +79,43 @@ class Player {
 			this.moveRight()
 		}
 	}
+
+	check(){
+		if (this.col === game.treasure.col && this.row === game.treasure.row) {
+			 clear()
+			 console.log('cats')
+			 game.treasure.treasureReset()
+			 this.score += 1;
+			 console.log(this.score)
+			 scoreCount.innerText = 'Score: ' + this.score;
+		}
+	}
+
+	// scoreCount(){
+	// 	let score = 0
+	// 	if (this.col === game.treasure.col && this.row === game.treasure.row) {
+	// 		score + 1
+	//    }
 }
+
+class Treasure {
+	constructor() {
+		// this.row = Math.floor(Math.random() * 9)
+		// this.col = Math.floor(Math.random() * 9)
+		this.image
+	}
+
+	treasureReset() {
+		this.row = Math.floor(Math.random() * 9)
+		this.col = Math.floor(Math.random() * 9)
+	}
+	
+	draw(){
+		image(this.image, 100*this.row, 100*this.col, 100, 100)
+	} 
+	// setRandomPosition() 
+}
+
 
 
 
