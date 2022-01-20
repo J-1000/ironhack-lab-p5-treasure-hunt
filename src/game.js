a9f0cbd58eb5dae89b2c9c746fa193bdc44996bd
Jonas Darley <jonasdarley@live.com>
2022-01-20 16:40:25 +0100

    The little guy is not showing up

--- src/game.js
+++ src/game.js
@@ -1,7 +1,52 @@
 class Game {
-  drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    drawGrid() {   
+    let lineX = 100
+    let lineY = 100
+    for (let i = 0; i < 9; i++) {
+      line(lineX, 0, lineX, 1000)
+      lineX +=100
+    } 
+    for (let i = 0; i < 9; i++) {
+      line(0, lineY, 1000, lineY)
+      lineY +=100
+    } 
+    
+    
   }
+}
+
+
+
+class Player {
+  constructor() {
+  	this.x = 500
+		this.y = 500
+		this.width = 100
+		this.height = 100
+		this.image
+	}
+
+  preload() {
+    this.image = loadImage('character-down.png')
+  }
+
+
+	draw() {
+		image(this.image, this.x, this.y, this.width, this.height)
+		
+		
+	}
+	moveRight() {
+		this.x += 10
+	}
+	moveLeft() {
+		this.x -= 10
+	}
+	moveUp() {
+		this.y -= 10
+	}
+	moveDown() {
+		this.y += 10
+	}
+  
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 const game = new Game();
+const player = new Player();
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  //background(color(255, 204, 0));
 }
 
 function draw() {
   game.drawGrid();
+  player.draw();
+
 }
