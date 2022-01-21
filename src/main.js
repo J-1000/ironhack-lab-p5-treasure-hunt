17ff99ec5ba6cbf60205c766b97b8c50e3337e8e
Jan Gorecki <jasiekgorecki@gmail.com>
2022-01-21 21:42:36 +0100

    Solved Lab

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 const game = new Game();
+const player = new Player();
+const treasure = new Treasure();
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  treasure.setRandomPosition();
 }
 
-function draw() {
-  game.drawGrid();
+function preload(){
+  player.preload()
+  treasure.preload()
 }
+
+
+function draw() {
+  clear()
+  game.drawGrid();
+  player.drawPlayer();
+  treasure.drawTreasure();
+  
+  
+}
+
+
+function keyPressed() {
+  player.keyPressed()
+  //check collision
+
+}
+
+function updateScore() {
+  game.updateScore()}
