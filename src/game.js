4382b137ec3109f9ce9d768fe13f79f53d98129d
Areeba Imran <areebaimran98@gmail.com>
2022-01-21 15:19:55 +0100

    Done till iteration 4

--- src/game.js
+++ src/game.js
@@ -1,7 +1,68 @@
 class Game {
+
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+
+    let currentLineSpacing = 0
+    for(let i = 0; i < 11; i++){
+
+      line(0, currentLineSpacing, WIDTH, currentLineSpacing);
+      line(currentLineSpacing, 0, currentLineSpacing, HEIGHT)
+
+      currentLineSpacing += SQUARE_SIDE
+    }
   }
 }
+
+class Player{
+
+  constructor(col, row){
+
+    this.col = col
+    this.row = row 
+    this.currentPlayerImage
+    this.playerLeft
+    this.playerRight
+    this.playerUp
+    this.playerDown
+  }
+
+  moveUp(){
+
+    this.row -= 100
+  }
+
+  moveDown(){
+
+    this.row += 100
+  }
+
+  moveLeft(){
+
+    this.col -= 100
+  }
+
+  moveRight(){
+
+    this.col += 100
+  }
+
+  preload(){
+
+    this.currentPlayerImage = loadImage('assets/character-down.png')
+    this.playerLeft = loadImage('assets/character-left.png')
+    this.playerRight = loadImage('assets/character-right.png')
+    this.playerUp = loadImage('assets/character-up.png')
+    this.playerDown = loadImage('assets/character-down.png')
+  }
+
+  draw(){
+
+    image(this.currentPlayerImage, this.col, this.row, 100, 100)
+  }
+}
+
+class Treasure(){
+  
+}
+  
+ 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const game = new Game();
+let player = new Player(100,100)
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -7,4 +8,41 @@ function setup() {
 
 function draw() {
   game.drawGrid();
+  player.draw()
 }
+
+function preload(){
+
+  player.preload()
+}
+
+function keyPressed(){
+
+  clear()
+
+  if(keyCode === UP_ARROW){
+
+    player.moveUp()
+    player.currentPlayerImage = player.playerUp
+  }
+    
+    
+  else if(keyCode === DOWN_ARROW){
+
+    player.moveDown()
+    player.currentPlayerImage = player.playerDown
+  }
+  
+  else if(keyCode === LEFT_ARROW){
+
+    player.moveLeft()
+    player.currentPlayerImage = player.playerLeft
+  }
+
+  else if(keyCode === RIGHT_ARROW){
+
+    player.moveRight()
+    player.currentPlayerImage = player.playerRight
+  }
+}
+
