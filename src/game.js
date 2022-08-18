353c415a1af2d2ef3c1b59dbe35e288223fbdc88
Joanna Zielinska <joannazielinskam@gmail.com>
2022-08-18 16:18:40 +0200

    done

--- src/game.js
+++ src/game.js
@@ -1,7 +1,112 @@
 class Game {
-  drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+  constructor() {
+		this.player = new Player()
+    this.treasure = new Treasure()
+    this.treasure.setRandomPosition()
+    this.socre = 0
+	} 
+
+  drawGrid() {  
+    stroke('black')
+    for ( let x = 0; x <= WIDTH; x += SQUARE_SIDE) {
+      for ( let y = 0; y <= HEIGHT; y += SQUARE_SIDE)
+      line(0, y, WIDTH, y)
+      line(x , 0, x, HEIGHT)
+    } 
+  
+
   }
+  preload() {
+		this.player.imageDown = loadImage('../assets/character-down.png')
+    this.player.imageLeft = loadImage('../assets/character-left.png')
+    this.player.imageRight = loadImage('../assets/character-right.png')
+    this.player.imageUp = loadImage('../assets/character-up.png') 
+    this.player.image = this.player.imageDown
+
+    this.treasure.image = loadImage('../assets/treasure.png') 
+	}
+
+  draw() {    
+    this.player.draw()  
+  }
+  drawTreasure() {    
+    this.treasure.draw()
+  }
+
+positionChange() {
+
+    if (this.player.col === this.treasure.col && this.player.row === this.treasure.row){
+     this.treasure.setRandomPosition() 
+     this.socre ++    
+     console.log(this.socre)
+    }      
+  }   
+
 }
+
+class Player {
+	constructor() {
+		this.col = 0
+		this.row = 0 
+    this.image
+    this.imageDown
+    this.imageLeft
+		this.imageRight
+    this.imageUp
+
+	}
+  draw() {   
+    image(this.image, this.col + 10 , this.row + 10, 80, 80)           
+    }
+
+	moveRight() {
+    if( this.col < WIDTH -SQUARE_SIDE ){
+		  this.col += SQUARE_SIDE
+       this.image = this.imageRight
+    }
+	}
+	moveLeft() {
+    if( this.col > 0 ){
+		  this.col -= SQUARE_SIDE
+      this.image = this.imageLeft
+    }
+	}
+	moveUp() {
+    if ( this.row > 0 )
+    {
+		  this.row -= SQUARE_SIDE
+      this.image = this.imageUp
+    }
+	}
+	moveDown() {
+    if ( this.row < HEIGHT - SQUARE_SIDE ){
+		  this.row += SQUARE_SIDE
+      this.image = this.imageDown
+    }
+	}
+}
+
+class Treasure {
+  constructor() {
+		this.col 
+		this.row  
+    this.image
+  }
+
+  setRandomPosition(){    
+    this.col = Math.floor(Math.random()*10)*SQUARE_SIDE
+    this.row = Math.floor(Math.random()*10)*SQUARE_SIDE
+  }
+
+  
+  draw() {  
+    image(this.image, this.col , this.row, SQUARE_SIDE, SQUARE_SIDE)
+    game.positionChange()
+  }
+} 
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 const game = new Game();
 
+function preload() {
+	game.preload()
+}
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
@@ -7,4 +11,30 @@ function setup() {
 
 function draw() {
   game.drawGrid();
+  game.draw() 
+  game.drawTreasure()
+  
 }
+
+
+function keyPressed() {
+  if (keyCode === 39) {
+    clear()  
+  game.player.moveRight()
+  }
+  if (keyCode === 37) {
+    clear()
+  game.player.moveLeft()
+  }
+  if (keyCode === 38) {
+    clear()
+  game.player.moveUp()
+  }
+  if (keyCode === 40) {
+    clear()
+  game.player.moveDown()
+  }
+}
+
+
+
