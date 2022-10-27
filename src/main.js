eea7b9185948e670da70c07b80eaaa2b70fc12f6
Fiodor Gorobet <casusarts@gmail.com>
2022-10-27 17:57:27 +0200

    Solved lab (no bonuses)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 const game = new Game();
 
+function preload() {
+  game.preload();
+}
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  frameRate(FPS)
 }
 
 function draw() {
-  game.drawGrid();
+  game.draw();
+  game.move();
+  game.over();
 }
+
