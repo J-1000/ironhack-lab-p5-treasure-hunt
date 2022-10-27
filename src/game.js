b7be2b17218ee5220b89ccb54d2067af079cfd05
Iwona Behnke <behnke.iwona@gmail.com>
2022-10-27 16:58:36 +0200

    Player moving on a playfield

--- src/game.js
+++ src/game.js
@@ -1,14 +1,46 @@
 class Game {
-  drawGrid() {
-    const numberOfRows = 10
-    const numberOfColumns = 10
-    const step = SQUARE_SIDE
-    const positions = []
+  constructor() {
+    this.player = new Player()
+  }
 
+  drawGrid() {
+    clear()
+    const step = SQUARE_SIDE
     // https://editor.p5js.org/rustyrobison/sketches/TFzek4T1v
     for (let i = 0; i <= WIDTH; i += step) {
       line(i, 0, i, HEIGHT);
       line(WIDTH, i, 0, i);
     }
+    this.player.draw()
+  }
+}
+
+class Player {
+  constructor(col,row) {
+    this.col = 0
+    this.row = 0
+    this.image
+    this.width = 100
+		this.height = 100
+  }
+
+  moveUp() {
+    this.row -= SQUARE_SIDE
+  }
+
+  moveDown() {
+    this.row += SQUARE_SIDE
+  }
+
+  moveLeft() {
+    this.col -= SQUARE_SIDE
+  }
+
+  moveRight() {
+    this.col += SQUARE_SIDE
+  }
+
+  draw() {
+    image(this.image, this.col, this.row, this.width, this.height)
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 const game = new Game();
 
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
 }
 
+function preload() {
+  game.player.image = loadImage("./../assets/character-down.png")
+}
+
 function draw() {
   game.drawGrid();
+  game.player.draw();
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case 38:
+      game.player.moveUp()
+      break;
+    case 40:
+      game.player.moveDown()
+      break;
+    case 37:
+      game.player.moveLeft()
+      break;
+    case 39:
+      game.player.moveRight()
+      break;
+    default:
+      null;
+  }
 }
