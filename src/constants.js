3d4d26d6bf7b7db3c1ac8192348868d4a4f0d70e
Micaela Rosadio <rosadio.karen@gmail.com>
2022-10-28 00:02:52 +0200

    Complete mandatory lab tasks

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 const WIDTH = 1000;
 const HEIGHT = WIDTH;
 const SQUARE_SIDE = WIDTH / 10;
+
+
--- src/game.js
+++ src/game.js
@@ -1,7 +1,108 @@
+
+/* ------------------------------------- */
+/* ------------ Class Game ------------- */
+/* ------------------------------------- */
+
 class Game {
+
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    let position = 0;
+    while (position <= WIDTH) {
+      line(position, 0, position, HEIGHT); 
+      line(0, position, WIDTH, position);
+      position += SQUARE_SIDE;
+    }
+  }
+
+}
+
+/* --------------------------------------- */
+/* ------------ Class Player ------------- */
+/* --------------------------------------- */
+
+class Player {
+
+  constructor(row, col) {
+    this.row = row;
+    this.col = col; 
+    this.image;
+  }
+
+  preload() {
+    this.imageDown  = loadImage("../assets/character-down.png");
+    this.imageLeft  = loadImage("../assets/character-left.png");
+    this.imageRight = loadImage("../assets/character-right.png");
+    this.imageUp    = loadImage("../assets/character-up.png"); 
+    this.image      = this.imageDown;
+  }
+
+  draw() {
+      image(this.image, this.row, this.col, SQUARE_SIDE, SQUARE_SIDE);
+  }
+
+  moveUp() {
+    this.col -= SQUARE_SIDE;
+    this.image = this.imageUp;
+  }
+
+  moveDown() {
+    this.col += SQUARE_SIDE;
+    this.image = this.imageDown;
+  }
+
+  moveLeft() {
+    this.row -= SQUARE_SIDE;
+    this.image = this.imageLeft;
+  }
+
+  moveRight() {
+    this.row += SQUARE_SIDE;
+    this.image = this.imageRight;
+  }
+  
+  keyPressed() {
+    if (keyCode === 37) {
+        this.moveLeft();
+    } if (keyCode === 39) {
+        this.moveRight();
+    } if (keyCode === 38) {
+        this.moveUp();
+    } if (keyCode === 40) {
+        this.moveDown();
+    }
   }
 }
+
+
+/* --------------------------------------- */
+/* ----------- Class Treasure ------------ */
+/* --------------------------------------- */
+
+class Treasure {
+
+  constructor() {
+    this.col;
+    this.row;
+    this.image;
+  }
+
+  preload() {
+    this.image = loadImage("../assets/treasure.png");
+  }
+
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * 10) * 100;
+    this.row = Math.floor(Math.random() * 10) * 100;
+  }
+
+  draw() {
+    image(this.image, this.row, this.col, SQUARE_SIDE, SQUARE_SIDE);
+    
+    if (player.row === treasure.row && player.col === treasure.col) {
+      treasure.setRandomPosition();
+    }
+  }
+
+}
+
+
