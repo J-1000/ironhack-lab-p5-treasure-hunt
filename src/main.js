cee671e2e0b80ae83877b7cbb0a5bcb06023fadb
Ezequiel Benitez <ezequielbv@gmail.com>
2022-10-28 09:41:23 +0200

    [BER OCT22 FT] - Ezequiel Done

--- src/main.js
+++ src/main.js
@@ -5,6 +5,27 @@ function setup() {
   canvas.parent("canvas");
 }
 
+function preload() {
+  game.preload();
+}
+
 function draw() {
   game.drawGrid();
+  game.player.draw();
+  game.treasure.draw();
+}
+
+function keyPressed() {
+  if (keyCode === 37) {
+    game.player.moveLeft();
+  }
+  if(keyCode === 39) {
+    game.player.moveRight();
+  }
+  if (keyCode === 38) {
+    game.player.moveUp();
+  }
+  if (keyCode === 40) {
+    game.player.moveDown();
+  }
 }
